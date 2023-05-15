9e68f640c67b680b2b024cac38cafe7a2745f948
Shahlar95 <asahlar3@gmail.com>
2023-05-15 11:30:21 +0400

    backend/reduxtoolkit/createsliece/

---
+++ frontend/chat-ecommerce-frontend/src/services/appApi.js
@@ -0,0 +1,31 @@
+import {createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+// create the api
+
+export const appApi = createApi({
+    reducerPath: "appApi",
+    baseQuery: fetchBaseQuery({baseUrl:"http://localhost8080"}),
+    endpoints: (builder) => ({
+        signup: builder.mutation({
+            query: (user) =>({
+                url: "/users/signup",
+                method: "POST",
+                body: user,
+    }),
+  }),
+        login: builder.mutation({
+            query: (user) => ({
+                url: "/users/login",
+                method: "POST",
+                bodu: user,
+            }),
+        }),
+
+}),
+})
+
+
+export const { useSignupMutation, useLoginMutation } = appApi;
+
+export default appApi;
