b4f405dd6eac60d849dc5062dd1bf5eb75e260fa
Shahlar95 <asahlar3@gmail.com>
2023-05-18 21:59:53 +0400

    Signup/add/handle/

--- frontend/chat-ecommerce-frontend/src/page/Signup.js
+++ frontend/chat-ecommerce-frontend/src/page/Signup.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Col, Container, Form, Row, Button} from 'react-bootstrap';
+import {Col, Container, Form, Row, Button, Alert} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import './Signup.css';
 import { useSignupMutation} from '../services/appApi'
@@ -9,20 +9,21 @@ const Signup = () => {
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
     const [name,setName] = useState('');
-   const [signupm, {error, isLoading, isError}] = useSignupMutation();
+   const [signup, {error, isLoading, isError}] = useSignupMutation();
 
 
-    function handleSubmit(){
-
+    function handleSignup(e){
+        e.preventDefault();
+        signup({name,email, password});
     }
 
   return (
     <Container>
     <Row>
         <Col md={6} className='sign__form--container '>
-            <Form style ={{width: "100%"}}>
+            <Form style ={{width: "100%"}} onSubmit= {handleSignup}>
                 <h1>Create an  account</h1>
-                {isError && <Alert/>}
+                {isError && <Alert variant='danger' >{error.data}</Alert>}
                 <Form.Group>
                     <Form.Label>Name</Form.Label>
                     <Form.Control type='text' placeholder='Your name' value={name}  required onChange={(e) => setName(e.target.value)}/>
@@ -36,9 +37,11 @@ const Signup = () => {
                     <Form.Control type='password' placeholder='Enter password' value={password} required onChange={(e)=> setPassword(e.target.value)}/>
                 </Form.Group>
                 <Form.Group>
-                    <Button type='submit'>Login</Button>
+                    <Button type='submit' disabled={isLoading}
+                    
+                    >Login</Button>
                 </Form.Group>
-                <p>Don't haven an account? <Link to='/signup'>Create account</Link>{''}
+                <p>Don't haven an account? <Link to='/login'>Login</Link>{''}
                 </p>
             </Form>
         </Col>
