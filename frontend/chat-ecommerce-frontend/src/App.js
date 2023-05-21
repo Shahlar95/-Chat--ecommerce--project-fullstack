6734aebbcb13ecd1abb102eb954828a26ada1773
Shahlar95 <asahlar3@gmail.com>
2023-05-21 23:41:07 +0400

    Newproduct/add/page/appjs/login/page/correction

--- frontend/chat-ecommerce-frontend/src/App.js
+++ frontend/chat-ecommerce-frontend/src/App.js
@@ -5,17 +5,25 @@ import Navigation from './components/Navigation';
 import Home from './page/Home';
 import Login from './page/Login';
 import Signup from './page/Signup';
+import { useSelector } from 'react-redux';
 
 
 function App() {
+  const user = useSelector((state)=> state.user);
+
   return (
     <div className="App">
       <BrowserRouter>
       <Navigation/>
       <Routes>
         <Route index element = {<Home/>}/>
+        {!user && 
+        <>
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<Signup/>}/>
+        
+        </>}
+        
         <Route path='*' element={<Home/>}/>
       </Routes>
       </BrowserRouter>
--- frontend/chat-ecommerce-frontend/src/page/Login.js
+++ frontend/chat-ecommerce-frontend/src/page/Login.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import {Col, Container, Form, Row, Button} from 'react-bootstrap';
+import {Col, Container, Form, Row, Button, Alert} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import { useLoginMutation } from "../services/appApi";
 import "./Signup.css";
@@ -19,6 +19,7 @@ const Login = () => {
         <Col md={6} className='login__form--container'>
             <Form style ={{width: "100%"}}  onSubmit={handleLogin}>
                 <h1>Login to your account</h1>
+                {isError && <Alert variant="danger">{error.data}</Alert> }
                 <Form.Group>
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control type='email' placeholder='Enter email' value={email}  required onChange={(e) => setEmail(e.target.value)}/>
@@ -28,7 +29,7 @@ const Login = () => {
                     <Form.Control type='password' placeholder='Enter password' value={password} required onChange={(e)=> setPassword(e.target.value)}/>
                 </Form.Group>
                 <Form.Group>
-                    <Button type='submit'>Login</Button>
+                    <Button type='submit' disabled={isLoading}>Login</Button>
                 </Form.Group>
                 <p>Don't haven an account? <Link to='/signup'>Create account</Link>{''}
                 </p>
