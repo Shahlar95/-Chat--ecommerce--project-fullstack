a449bf8a96e00fc4353c726de2c4ca501927e39e
Shahlar95 <asahlar3@gmail.com>
2023-05-31 14:55:49 +0400

    add/loading/similiarproduct/add/productpage

--- frontend/chat-ecommerce-frontend/src/page/ProductPage.js
+++ frontend/chat-ecommerce-frontend/src/page/ProductPage.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import AliceCarousel from 'react-alice-carousel';
 import axios from '../axios';
+import Loading from '../components/Loading';
 
 function ProductPage() {
     const {id} = useParams();
@@ -18,12 +19,34 @@ function ProductPage() {
         })
     }, [id])
     const images = product.pictures.map((picture) => <img className='product__carousel-image' src={picture.url} onDragStart={handleDragStart}/>) 
+    if(!product){
+        return  <Loading/>
+    }
+
+    let similarProducts = [];
+     if(similar){
+        similarProducts = similar.map((product, idx) => {
+            <div className='item' data-value={idx}>
+                <SimilarProduct {...product}/> 
+            </div>
+        })
+     }
 
   return (
     <Container className='pt-4' style = {{position:'relative'}}>
         <Row>
             <Col lg={6}>
-                <AliceCarousel mouseTracking items{images} controlsStrategy = 'alternative'
+                <AliceCarousel mouseTracking items = {images} controlsStrategy = 'alternative'/>
+            </Col>
+            <Col lg={6} className='pt-4'>
+                <h1>{product.name}</h1>
+                <p>
+                    <Badge bg="primary">{product.category}</Badge>
+                </p>
+                <p className='product__price'>${product.price}</p>
+                <p style={{textAlign:'justify'}} className='py-3'>
+                <strong>Description:</strong> {product.description}   
+                 </p>
             </Col>
         </Row>
     </Container>
