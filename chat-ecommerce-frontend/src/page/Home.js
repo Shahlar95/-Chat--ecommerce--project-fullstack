63b14a9b6ff150d6faa87e34423d9cec6ba0b294
Shahlar95 <asahlar3@gmail.com>
2023-07-29 21:10:09 +0400

    problem/error/homerpage/slice

--- chat-ecommerce-frontend/src/page/Home.js
+++ chat-ecommerce-frontend/src/page/Home.js
@@ -12,8 +12,9 @@ import axios from '../axios'
 const Home = () => {
   const dispatch = useDispatch();
   const products = useSelector(state => state.products);
+  console.log(products)
   const lastProducts = products.slice(0, 8);
- 
+
 useEffect(()=>{
   axios.get('/products').then(({data}) => dispatch(updateProducts(data)))
 },[]);
@@ -44,7 +45,6 @@ useEffect(()=>{
       <h2>Categories</h2>
       <Row>
        {categories.map((category)=>(
-        
         <LinkContainer  key={category.name} to={`/category/${category.name.toLocaleLowerCase()}`}>
         <Col   key={category.name}  md={4}>
         <div style={{backgroundImage:`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${category.img})`, gap:"10px"}} className='category-tile'>
