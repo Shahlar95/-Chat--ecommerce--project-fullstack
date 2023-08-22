f46d02983229a9386e76332d168e43be786e32ae
Shahlar95 <asahlar3@gmail.com>
2023-08-22 12:01:57 +0400

    correct/navigation/orderpage

--- chat-ecommerce-frontend/src/components/Navigation.js
+++ chat-ecommerce-frontend/src/components/Navigation.js
@@ -30,7 +30,7 @@ function Navigation() {
     }
 
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar className='navbar'  expand="lg">
             <Container>
                 <LinkContainer to="/">
                     <Navbar.Brand>Ecomern</Navbar.Brand>
