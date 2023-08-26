dc3976d1a319a16c1eea1a9dea3812b75fd41fae
Shahlar95 <asahlar3@gmail.com>
2023-08-26 15:10:50 +0400

    correct/footer/home

--- chat-ecommerce-frontend/src/components/Footer.js
+++ chat-ecommerce-frontend/src/components/Footer.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import './Footer.css';
 
 export default function Footer() {
   return (
     <MDBFooter bgColor='primary' className='text-center text-lg-start text-muted'>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
         <div className='me-5 d-none d-lg-block'>
-          <span>Get connected with us on social networks:</span>
+          <span id='footer-text'>Get connected with us on social networks:</span>
         </div>
 
         <div>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='facebook-f' />
+          <a href=''  className='me-4 text-reset'>
+            <MDBIcon id='footer-text' color='secondary' fab icon='facebook-f' />
           </a>
           <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='twitter' />
+            <MDBIcon id='footer-text' color='secondary' fab icon='twitter' />
           </a>
           <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='google' />
+            <MDBIcon id='footer-text' color='secondary' fab icon='google' />
           </a>
           <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='instagram' />
+            <MDBIcon id='footer-text' color='secondary' fab icon='instagram' />
           </a>
           <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='linkedin' />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='github' />
+            <MDBIcon id='footer-text' color='secondary' fab icon='linkedin' />
           </a>
         </div>
       </section>
@@ -35,90 +33,56 @@ export default function Footer() {
         <MDBContainer className='text-center text-md-start mt-5'>
           <MDBRow className='mt-3'>
             <MDBCol md='3' lg='4' xl='3' className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon color='secondary' icon='gem' className='me-3' />
-                Company name
+              <h6 id='footer-text' className='text-uppercase fw-bold mb-4'>
+                <MDBIcon id='footer-text' color='secondary' icon='gem' className='me-3' />
+                E-commerce
               </h6>
-              <p>
+              <p id='footer-text'>
                 Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit
                 amet, consectetur adipisicing elit.
               </p>
             </MDBCol>
 
             <MDBCol md='2' lg='2' xl='2' className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+              <h6 id='footer-text' className='text-uppercase fw-bold mb-4'>Products</h6>
               <p>
-                <a href='#!' className='text-reset'>
-                  Angular
-                </a>
+               
               </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  React
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Vue
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Laravel
-                </a>
-              </p>
-            </MDBCol>
-
-            <MDBCol md='3' lg='2' xl='2' className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Pricing
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Settings
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
+              <p id='footer-text'>
+                <a href='/orders' className='text-reset'>
                   Orders
                 </a>
               </p>
-              <p>
-                <a href='#!' className='text-reset'>
+              <p id='footer-text'>
+                <a href='/' className='text-reset'>
                   Help
                 </a>
               </p>
             </MDBCol>
 
             <MDBCol md='4' lg='3' xl='3' className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-              <p>
-                <MDBIcon color='secondary' icon='home' className='me-2' />
+              <h6 id='footer-text' className='text-uppercase fw-bold mb-4'>Contact</h6>
+              <p id='footer-text'>
+                <MDBIcon id='footer-text' color='secondary' icon='home' className='me-2' />
                 New York, NY 10012, US
               </p>
-              <p>
-                <MDBIcon color='secondary' icon='envelope' className='me-3' />
+              <p id='footer-text'>
+                <MDBIcon id='footer-text' color='secondary' icon='envelope' className='me-3' />
                 info@example.com
               </p>
-              <p>
-                <MDBIcon color='secondary' icon='phone' className='me-3' /> + 01 234 567 88
+              <p id='footer-text'>
+                <MDBIcon id='footer-text' color='secondary' icon='phone' className='me-3' /> + 01 234 567 88
               </p>
-              <p>
-                <MDBIcon color='secondary' icon='print' className='me-3' /> + 01 234 567 89
+              <p id='footer-text'>
+                <MDBIcon id='footer-text' color='secondary' icon='print' className='me-3' /> + 01 234 567 89
               </p>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
       </section>
 
-      <div className='text-center p-4'>
-        © 2021 Copyright:
-        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
-        </a>
+      <div  id='footer-text' className='text-center p-4'>
+        © 2023 Copyright
       </div>
     </MDBFooter>
   );
--- chat-ecommerce-frontend/src/page/Home.js
+++ chat-ecommerce-frontend/src/page/Home.js
@@ -1,63 +1,101 @@
-import React, {useEffect} from 'react';
-import {Row,Col} from 'react-bootstrap';
-import { LinkContainer } from "react-router-bootstrap";
-import {Link} from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 import categories from '../categories';
-import "./Home.css";
+import './Home.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateProducts } from '../features/productSlice';
 import ProductPreview from '../components/ProductPreview';
-import axios from '../axios'
+import axios from '../axios';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const products = useSelector(state => state.products);
-  const lastProducts =  products ? products.slice(0, 8) : [];
+  const products = useSelector((state) => state.products);
+  const lastProducts = products ? products.slice(0, 8) : [];
 
-useEffect(()=>{
-  axios.get('/products').then(({data}) => dispatch(updateProducts(data)))
-},[]);
-
+  useEffect(() => {
+    axios.get('/products').then(({ data }) => dispatch(updateProducts(data)));
+  }, []);
 
   return (
     <div>
-      <img src="https://res.cloudinary.com/learn-code-10/image/upload/v1653947013/yqajnhqf7usk56zkwqi5.png" alt="img" className='home-banner'/>
-      <div className='featured-products-container mt-4'>
+      {/* <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100vh',
+          paddingTop: '100.0000%',
+          paddingBottom: 0,
+          boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)',
+          overflow: 'hidden',
+          borderRadius: '8px',
+          willChange: 'transform',
+        }}
+      >
+        <iframe
+          title="Canva Design"
+          loading="lazy"
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100vh',
+            border: 'none',
+           
+          }}
+          src="https://www.canva.com/design/DAFsby-o06w/view?embed"
+          allowFullScreen
+          allow="fullscreen"
+        ></iframe>
+      </div> */}
+      <img
+        src="https://www.canva.com/design/DAFsby-o06w/KY4YWIXmhY_GxVE5X0NwyQ/view?utm_content=DAFsby-o06w&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+        alt="img"
+        className="home-banner"
+      />
+      <div className="featured-products-container mt-4">
         <h2>Last products</h2>
         {/* last products here */}
-        <div className='d-flex justify-content-center flex-wrap'>
-         {lastProducts.map((product)=>(
-          <ProductPreview {...product}/>
-        ))}
+        <div className="d-flex justify-content-center flex-wrap">
+          {lastProducts.map((product) => (
+            <ProductPreview key={product.id} {...product} />
+          ))}
         </div>
         <div>
-        <Link to="/category/all" style={{textAlign:"center", display:"block", textDecoration:"none"}}>
-          See more {">>"}
-        </Link>
+          <Link
+            to="/category/all"
+            style={{ textAlign: 'center', display: 'block', textDecoration: 'none' }}
+          >
+            See more &gt;&gt;
+          </Link>
+        </div>
+      </div>
+      {/* sale banner */}
+      <div className="sale__banner--container mt-4">
+        <img src="../../assets/image/image2.png" alt="img" />
+      </div>
+      <div className="recent-products-container mt-4">
+        <h2>Categories</h2>
+        <Row>
+          {categories.map((category) => (
+            <LinkContainer key={category.name} to={`/category/${category.name.toLowerCase()}`}>
+              <Col key={category.name} md={4}>
+                <div
+                  style={{
+                    backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${category.img})`,
+                    gap: '10px',
+                  }}
+                  className="category-tile"
+                >
+                  {category.name}
+                </div>
+              </Col>
+            </LinkContainer>
+          ))}
+        </Row>
       </div>
     </div>
-    {/* sale banner */}
-    <div className="sale__banner--container mt-4">
-    <img src=" https://res.cloudinary.com/learn-code-10/image/upload/v1654093280/xkia6f13xxlk5xvvb5ed.png"  alt="img"/>
-    </div>
-    <div className='recent-products-container mt-4'>
-      <h2>Categories</h2>
-      <Row>
-       {categories.map((category)=>(
-        <LinkContainer  key={category.name} to={`/category/${category.name.toLocaleLowerCase()}`}>
-        <Col   key={category.name}  md={4}>
-        <div style={{backgroundImage:`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${category.img})`, gap:"10px"}} className='category-tile'>
-        {category.name}
-        </div>
-        </Col>
-        </LinkContainer>
-       ))} 
-      </Row>
+  );
+};
 
-    </div>
-    
-    </div>
-)
-}
-
-export default Home
+export default Home;
