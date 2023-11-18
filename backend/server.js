dec0e6f74dae9a9849ccbe5972f7025f50737353
unknown <asahlar3@gmail.com>
2023-11-18 21:32:41 +0400

    add/baseurls/correct

--- backend/server.js
+++ backend/server.js
@@ -21,13 +21,7 @@ const imageRoutes = require('./routes/imageRoutes');
 
  
 
-app.use(cors(
-    {
-        origin: ["https://deploy-mern-lwhq.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials:true
-    }
-));
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use('/users', userRoutes);
